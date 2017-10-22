f22075afb90072c0b5d96b51eff0ec652bc1402e
nguyenle510 <phungchithien96@gmail.com>
2017-10-22 22:53:32 +0700

    buoi 4

---
+++ PhungChiThien-b4/Bai2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ipAdress;
+            int port;
+            string protocol;
+            Console.Write("Nhap host ip: ");
+            ipAdress = Console.ReadLine();
+            Console.Write("Nhap host port: ");
+            port = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap protocol: ");
+            protocol = Console.ReadLine();
+
+            switch (protocol)
+            {
+                case "TCP":
+                    IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+                    TcpClient client = new TcpClient();
+                    try
+                    {
+                        client.Connect(ip);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("host: {0}:{1} is close", ipAdress, port);
+                        Console.WriteLine(ex);
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("host: {0}:{1} is open", ipAdress, port);
+                    break;
+
+                case "UDP":
+                    IPEndPoint ipUDP = new IPEndPoint(IPAddress.Parse(ipAdress), port);
+                    UdpClient clientUDP = new UdpClient();
+                    try
+                    {
+                        clientUDP.Connect(ipUDP);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("host: {0}:{1} is close", ipAdress, port);
+                        Console.WriteLine(ex);
+                        Environment.Exit(0);
+                    }
+                    Console.WriteLine("host: {0}:{1} is open", ipAdress, port);
+                    break;
+            }
+        }
+    }
+}
---
+++ PhungChiThien-b4/Bai4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Bai4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int recv;
+            byte[] data = new byte[1024];
+
+            TcpListener server = new TcpListener(9050);
+            server.Start();
+
+            Console.WriteLine("waiting for client...");
+            TcpClient client = server.AcceptTcpClient();
+            NetworkStream ns = client.GetStream();
+
+            Console.Write("chon lenh: ");
+            string cmd = Console.ReadLine();
+            data = Encoding.ASCII.GetBytes(cmd);
+            ns.Write(data, 0, data.Length);
+        }
+    }
+}
